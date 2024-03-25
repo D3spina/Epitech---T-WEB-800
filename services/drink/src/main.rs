c39ab179ed461fff8684d439dac5224c688b26b0
GuillaumeGANGLOFF <148479543+GuillaumeGANGLOFF@users.noreply.github.com>
2024-03-25 14:51:31 +0100

    change to add more type place in research

--- services/drink/src/main.rs
+++ services/drink/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use serde_json::Value;
 
 #[macro_use]
@@ -6,11 +7,26 @@ extern crate common;
 use common::google::nearly_place_model::exploit_json;
 use common::google::Google;
 
-// URL pour récupérer les bar dans un périmétre donné et pour une localisation donnée
+
 #[get("/service/drink/<localisation>/<radius>")]
-async fn index(localisation: String, radius: i32) -> String {
-    let bar = get_google(localisation, radius).await;
+/*async fn index(localisation: String, radius: i32) -> String {
+    let bar = get_google(localisation, radius, types).await;
     let result = exploit_json(bar).unwrap();
+    format!("{:?}", result)
+}*/
+async fn index(localisation: String, radius: i32) -> String {
+    let types = ["bar", "cafe", "night_club"];
+    //crer un vecteur vide
+    let mut res = HashMap::new();
+
+    for &element in types.iter() {
+        let google = get_google(localisation.clone(), radius, String::from(element)).await;
+        let google_result = exploit_json(google).unwrap();
+        res.insert(element, google_result);
+    }
+
+    let result = serde_json::to_value(&res).unwrap();
+
     format!("{:?}", result)
 }
 
@@ -19,17 +35,17 @@ fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
 }
 
-pub(crate) async fn get_google(localisation: String, radius: i32) -> Value {
+pub(crate) async fn get_google(localisation: String, radius: i32, types: String) -> Value {
     let mut google = Google::new();
     google
         .geocoding(localisation)
         .await
         .expect("Geocoding n'a pas été éxécuté correctement");
-    let resto: Value = google
-        .nearby_place(String::from("bar"), radius)
+    let result =  google
+        .nearby_place(types, radius)
         .await
-        .expect("FDP");
-    resto
+        .expect("Erreur dans la récupération des données dans la fonction get_google");
+    result
 }
 
 
