51a30119abfe4a28e65638b275e7a5627a2afd51
NewTokyo <113348179+ymunsch8@users.noreply.github.com>
2024-04-15 12:00:45 +0200

    save

--- common/src/auth/mod.rs
+++ common/src/auth/mod.rs
@@ -1,40 +1,95 @@
-struct AuthToken(String);
+use std::collections::HashMap;
+use jsonwebtoken::{encode, decode, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Serialize, Deserialize};
+use crate::db::Database;
 
-#[rocket::async_trait]
-impl<'r> FromDataSimple for AuthToken {
-    type Error = String;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Login {
+    pub email:String,
+    pub password:String,
+}
 
-    async fn from_data(_: &'r Request<'_>, data: Data) -> data::Outcome<Self, Self::Error> {
-        let token = match data.open(1024 * 16).into_string().await {
-            Ok(token) => token,
-            Err(_) => return Outcome::Failure((Status::BadRequest, "Invalid token format".to_string())),
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Register {
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub password: String,
+    pub secure_password: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    email: String,
+    password: String,
+    exp: usize,
+}
+
+impl Claims {
+    fn generate_jwt(email: String, password: String) -> HashMap<String, String> {
+        let mut res: HashMap<String, String> = HashMap::new();
+        // Définition des revendications du JWT
+        let claims = Claims {
+            email : email.clone(),
+            password : password.clone(),
+            exp: (chrono::Utc::now() + chrono::Duration::hours(24)).timestamp() as usize, // Expiration dans 24 heures
         };
-        Outcome::Success(AuthToken(token))
+
+        // Clé secrète pour signer le JWT
+        let secret = "your_secret_key";
+
+        // Encodage du JWT
+        let token = encode(&Header::default(), &claims, &EncodingKey::from_secret(secret.as_ref()));
+
+        let valid = Database::query(&format!("Select email in user where email='{email}' and password='{password}'",email = email,password = password));
+
+        if(valid.is_err()){
+            res.insert("error".to_string(),"account not found".to_string());
+            return res;
+        }
+
+        res.insert("token".to_string(),token.unwrap());
+
+        res
     }
-}
 
-fn generate_jwt() -> Result<String, jsonwebtoken::errors::Error> {
-    // Vos données à inclure dans le JWT
-    let claims = jsonwebtoken::Claims::default();
+    pub fn generate_jwt_login(email: String, password: String) -> HashMap<String, String> {
+        // Définition des revendications du JWT
+        return Self::generate_jwt(email,password);
+    }
 
-    // Clé secrète pour signer le JWT
-    let secret = "your_secret_key";
+    pub fn generate_jwt_register(first_name:String, last_name:String, email: String, password: String, secure_password:String) -> HashMap<String, String> {
+        // Définition des revendications du JWT
+        let mut res: HashMap<String, String> = HashMap::new();
 
-    // Encodage du JWT
-    let token = encode(&Header::default(), &claims, &EncodingKey::from_secret(secret.as_ref()))?;
+        let valid;
+        if password == secure_password {
+               valid = Database::query(&format!("INSERT INTO `user`(`first_name`, `last_name`, `email`, `password`) VALUES ('{first_name}','{last_name}','{email}','{password}')",first_name = first_name,last_name=last_name,email=email,password=password));
+        }else {
+            res.insert("error".to_string(),"password is not same".to_string());
+        }
 
-    Ok(token)
-}
+        if valid.is_ok() {
+            return Self::generate_jwt(email,password);
+        }
 
-fn verify_token(token: &str) -> Result<(), String> {
-    let token_data = decode::<String>(
-        &token,
-        &DecodingKey::from_secret("your_secret_key".as_ref()),
-        &Validation::default(),
-    );
+        {"Error":"Password is not same".to_string()}
 
-    match token_data {
-        Ok(_) => Ok(()),
-        Err(_) => Err("Invalid token".to_string()),
+
+    }
+
+    // Fonction pour vérifier un JWT
+    pub fn verify_jwt(token: &str) -> Result<Claims, jsonwebtoken::errors::Error> {
+        // Clé secrète pour déchiffrer le JWT
+        let secret = "your_secret_key";
+
+        // Validation et décodage du JWT
+        let token_data = decode::<Claims>(
+            &token,
+            &DecodingKey::from_secret(secret.as_ref()),
+            &Validation::default(),
+        )?;
+
+        Ok(token_data.claims)
     }
 }
--- services/auth/src/main.rs
+++ services/auth/src/main.rs
@@ -5,20 +5,48 @@ use rocket::serde::json::Json;
 extern crate rocket;
 extern crate common;
 use std::collections::HashMap;
-use common::auth;
+use common::auth::{Claims, Login, Register};
 
 // URL pour récupérer les bar dans un périmétre donné et pour une localisation donnée
-#[post("/service/auth/login")]
-async fn login(/*localisation: String, radius: i32*/) -> Json<HashMap<String, String>> {
+#[post("/service/auth/login",format = "json",data = "<login>")]
+async fn login(login: Json<Login>) -> Json<HashMap<String, String>> {
     let mut res: HashMap<String, String> = HashMap::new();
+    let email = login.email.clone().parse().unwrap();
+    let password = login.password.clone().parse().unwrap();
+
+    let token = Claims::generate_jwt_login(email, password).unwrap();
+    //let token = Claims::new();
+
+    res.insert("token".parse().unwrap(), token);
 
     Json(res)
 }
 
 // URL pour récupérer les bar dans un périmétre donné et pour une localisation donnée
-#[post("/service/auth/register")]
-async fn register(/*localisation: String, radius: i32*/) -> Json<HashMap<String, String>> {
+#[post("/service/auth/register",format = "json",data = "<register>")]
+async fn register(register: Json<Register>) -> Json<HashMap<String, String>> {
     let mut res: HashMap<String, String> = HashMap::new();
+
+    let first_name = register.first_name.clone().parse().unwrap();
+    let last_name = register.last_name.clone().parse().unwrap();
+    let email = register.email.clone().parse().unwrap();
+    let password = register.password.clone().parse().unwrap();
+    let secure_password = register.secure_password.clone().parse().unwrap();
+
+
+    println!("test_other");
+
+    let token = Claims::generate_jwt_register(first_name,last_name,email, password,secure_password);
+
+    println!("test_other");
+
+    if token.is_ok(){
+        res.insert("token".to_string(),token.unwrap());
+    } else {
+        res.insert("error".to_string(),token.unwrap());
+    }
+
+    println!("{:?}",res);
 
     Json(res)
 }
